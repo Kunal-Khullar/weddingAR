cf22aae6e0fbfc62ea200cef380c9f31e8a7d16d
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-03-31 09:40:34 +0530

    dependencies

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import './product.css'
+import Button from 'react-bootstrap/Button'
 const Product = () => {
     return (
         <div>
+            <Button className="btn-primary"></Button>
             PRODUCT
         </div>
     )
--- src/components/working/Working.js
+++ src/components/working/Working.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './working.css'
 const Working = () => {
     return (
         <div>
