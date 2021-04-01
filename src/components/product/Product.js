993982c63afca42c1150244e184b3ed589e18651
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-01 09:22:03 +0530

    product container

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -3,6 +3,8 @@ import './product.css'
 import Button from 'react-bootstrap/Button'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
@@ -24,6 +26,20 @@ const Product = () => {
    
   </Navbar.Collapse>
 </Navbar>
+      <div className="top_container">
+        <Row>
+          <Col className="product_details col-md-8 ">
+            <Row>
+              logo 
+              product name
+            </Row>
+            <p>Details</p>
+            <Button>Buy Now</Button>
+          </Col>
+          <Col className="product_gif col-md-4 "></Col>
+        </Row>
+
+      </div>
         </div>
     )
 }
