57b368c2a72f31625b81164204b21c2b04f660c2
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-02 22:11:14 +0530

    hosting

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyANRRMnYduioYGua5pP46DdQfuuM3xa8g8",
+    authDomain: "weddingar-2021.firebaseapp.com",
+    projectId: "weddingar-2021",
+    storageBucket: "weddingar-2021.appspot.com",
+    messagingSenderId: "361428150741",
+    appId: "1:361428150741:web:6ca3d7dea4c5bd41baf55c",
+    measurementId: "G-VTCVGQ9TMF"
+  };
+  const firapp = firebase.initializeApp(firebaseConfig)
