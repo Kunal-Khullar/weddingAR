16bc50a8d6ba45f1dd28e37a6ea09f5b5b2ca2eb
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-02 21:17:10 +0530

    working

--- src/components/working/Working.js
+++ src/components/working/Working.js
@@ -6,6 +6,20 @@ const Working = () => {
         <div>
             <div id="works" className="mid_container">
                 <h1>How it works</h1>
+                <div className="work_container">
+                    <div className="steps">
+                    <img src="https://img.icons8.com/officel/2x/online-payment---v2.gif"/>
+                    <p>Book</p>
+                    </div>
+                    <div className="steps">
+                        <img src="https://img.icons8.com/office/2x/upload--v2.gif"></img>
+                        <p>Upload</p>
+                    </div>
+                    <div className="steps">
+                        <img src="https://img.icons8.com/color/2x/invitation--v2.gif"></img>
+                        <p>Invite</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
