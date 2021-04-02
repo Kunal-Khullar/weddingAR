043d5d037e0719bcc9ff0c54e3d50fef3c6f8c23
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-02 21:47:42 +0530

    working steps added

--- src/components/working/Working.js
+++ src/components/working/Working.js
@@ -7,17 +7,33 @@ const Working = () => {
             <div id="works" className="mid_container">
                 <h1>How it works</h1>
                 <div className="work_container">
-                    <div className="steps">
-                    <img src="https://img.icons8.com/officel/2x/online-payment---v2.gif"/>
-                    <p>Book</p>
+                    <div data-aos="fade-right"
+      data-aos-delay="50"
+      data-aos-duration="800"
+      data-aos-easing="ease-in-out-cubic" className="steps">
+                        <div className="imgcontainer">
+                    <img className="imgs" src="https://img.icons8.com/officel/2x/online-payment---v2.gif"/>
                     </div>
-                    <div className="steps">
-                        <img src="https://img.icons8.com/office/2x/upload--v2.gif"></img>
-                        <p>Upload</p>
+                    <h3 className="headings">Book</h3>
+                    <p className="steps_des">Book your models and upload your photos and invite your friends.</p>
                     </div>
-                    <div className="steps">
-                        <img src="https://img.icons8.com/color/2x/invitation--v2.gif"></img>
-                        <p>Invite</p>
+                    <div id="second" data-aos="fade-down"
+      data-aos-delay="50"
+      data-aos-duration="800"
+      data-aos-easing="ease-in-out-cubic" className="steps">
+                    <div className="imgcontainer">
+                        <img className="imgs" src="https://img.icons8.com/office/2x/upload--v2.gif" /></div>
+                        <h3 className="headings">Upload</h3>
+                        <p className="steps_des">Book your models and upload your photos and invite your friends.</p>
+                    </div>
+                    <div data-aos="fade-left"
+      data-aos-delay="50"
+      data-aos-duration="800"
+      data-aos-easing="ease-in-out-cubic" className="steps">
+                    <div className="imgcontainer">
+                        <img className="imgs" src="https://img.icons8.com/color/2x/invitation--v2.gif" /></div>
+                        <h3 className="headings">Invite</h3>
+                        <p className="steps_des">Book your models and upload your photos and invite your friends.</p>
                     </div>
                 </div>
             </div>
