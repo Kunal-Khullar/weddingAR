f9ce231bd06101ecaea66730d7837488be193428
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-02 14:28:14 +0530

    product gif

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -20,9 +20,13 @@ const Product = () => {
               <h1>Product Name</h1>
             </Row>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-            <Button className="btn1">Buy Now</Button>
+            <Button className="btn1 btn-success">Buy Now</Button>
           </Col>
-          <Col className="product_gif col-md-5 "></Col>
+          <Col className="product_gif col-md-5 ">
+            <video autoPlay muted loop>
+              <source src="https://cdn.dribbble.com/users/6745/screenshots/8195589/media/3d3e04346b2cf25c5efe9ceafcceb315.mp4" type="video/mp4" />
+            </video>
+          </Col>
         </Row>
 
       </div>
--- src/components/working/Working.js
+++ src/components/working/Working.js
@@ -3,7 +3,9 @@ import './working.css'
 const Working = () => {
     return (
         <div>
-            
+            <div className="mid_container">
+                <h1>How it works</h1>
+            </div>
         </div>
     )
 }
