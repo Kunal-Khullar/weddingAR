d2dfd5856eea4bf7084553d06275630d9615a8ba
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-04 13:49:32 +0530

    demo added

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -3,7 +3,11 @@ import './demo.css'
 const Demo = () => {
     return (
         <div>
-            
+            <div className="videoplayer">
+                <h1>Product Demo</h1>
+                <p>Here is a video showing the complete working of our product</p>
+                
+            </div>
         </div>
     )
 }
