040395ec0ea728993145375751db2c71ee1c793d
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-04 18:12:56 +0530

    video added

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -9,11 +9,16 @@ const Demo = () => {
         <div>
             <div className="videoplayer">
               
-         <div className="video">
+         <div className="video"
+            data-aos="fade-down"
+           data-aos-delay="50"
+           data-aos-duration="500"
+           data-aos-easing="ease-in-out-cubic"
+         >
          <h1>Product Demo</h1>
                 <p>Here is a video showing the complete working of our product</p>
            <div >
-           <video id="videos" autoPlay controls>
+           <video id="videos" poster="https://i.pinimg.com/originals/77/29/c3/7729c3395007be06ca06c35ccd799138.png"  controls>
              <source src="https://c4604efbcf2f76c02fd8-0c3ebf250b59f974a474a645a5bdda06.ssl.cf2.rackcdn.com/TrekkVideos/ForTrekkwWeb_Think%202016.mp4" type="video/mp4"></source>
            </video>
           </div>
