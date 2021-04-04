b9d123ef1c59ba7f05e3449164f70854dac5ed34
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-04 18:05:25 +0530

    video added

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -4,41 +4,20 @@ import { useVideojs } from 'react-videojs-hook';
  
 import 'video.js/dist/video-js.css';
 const Demo = () => {
-     const onPlay = (currentTime) => {
-    console.log("Video played at: ", currentTime);
-  };
  
-  const onPause = (currentTime) => {
-    console.log("Video paused at: ", currentTime);
-  };
- 
-  const onEnd = (currentTime) => {
-    console.log(`Video ended at ${currentTime}`);
-  };
- 
-  const onTimeUpdate = (currentTime) => {
-    console.log(`Video current time is ${currentTime}`)
-  };
- 
-    const { vjsId, vjsRef, vjsClassName } = useVideojs({
-        src: 'https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4',
-        controls: true,
-        autoplay: true,
-        responsive: true,
-        bigPlayButtonCentered: true,
-        onPlay,
-        onPause,
-        onEnd,
-        onTimeUpdate,
-      });
     return (
         <div>
             <div className="videoplayer">
-                <h1>Product Demo</h1>
+              
+         <div className="video">
+         <h1>Product Demo</h1>
                 <p>Here is a video showing the complete working of our product</p>
-                <div data-vjs-player>
-      <video ref={vjsRef} id={vjsId} className={vjsClassName}></video>
-    </div>
+           <div >
+           <video id="videos" autoPlay controls>
+             <source src="https://c4604efbcf2f76c02fd8-0c3ebf250b59f974a474a645a5bdda06.ssl.cf2.rackcdn.com/TrekkVideos/ForTrekkwWeb_Think%202016.mp4" type="video/mp4"></source>
+           </video>
+          </div>
+         </div>
             </div>
         </div>
     )
