e51750aadfd1f1e2d1f8a2b831083392c09d9d9d
Kunal Khullar <kunalkhullar5012@gmail.com>
2021-04-05 09:39:11 +0530

    app link

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './contact.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faApple} from '@fortawesome/free-solid-svg-icons'
+import { faGooglePlay} from '@fortawesome/free-solid-svg-icons'
 const Contact = () => {
     return (
         <div>
-            <div id="contacts"></div>
+            <div id="contacts">
+            <Row>
+                <Col id='leftcol'>
+                    <div className='applink'>
+                    <img src="https://img.icons8.com/officel/50/000000/mac-os.png"/>
+                    </div>
+                    <div className='applink'>
+                    <img src="https://img.icons8.com/dusk/64/000000/google-play.png"/>
+                    </div>
+                </Col>
+                <Col></Col>
+            </Row>
+            </div>
         </div>
     )
 }
